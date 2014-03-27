29fafa5f34941ae09116825a2dee0189ddd3b413
Carlos Folgar <folgar.tech@gmail.com>
2014-03-27 01:00:39 -0400

    slight change again

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ application.debug=True
 
 @application.route('/')
 def hello_world():
-    return "Hello Vincent! This is the VTHacks server."
+    return "Hello! This is the VTHacks server."
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
