be5ad6825127b113bfb21b8b8007bc0b352b97a0
Carlos Folgar <folgar.tech@gmail.com>
2014-03-28 13:52:40 -0400

    added placeholder way of getting credentials

--- application.py
+++ application.py
@@ -10,5 +10,15 @@ application.debug=True
 def hello_world():
     return "Hello! This is the VTHacks server."
 
+@application.route('/get_credentials')
+def get_credentials():
+    return 'this should return temporary credentials'
+
+    #response = _sts.get_federation_token(policy=policy)
+    #return {'accessKeyID': response.credentials.access_key,
+            #'secretAccessKey': response.credentials.secret_key,
+            #'securityToken': response.credentials.session_token,
+            #'expiration': response.credentials.expiration}
+
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
